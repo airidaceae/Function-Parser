fc53da0914ce79664915d2f1aecd8d5da8d9c4ed
Iris Pupo <pupoi@tutanota.com>
2022-09-28 13:43:34 -0400

    slimmed code in evaluate function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::str::Chars;
 use text_io;
 
 fn power(num1: f64, thing2: f64) -> f64{
@@ -12,46 +11,24 @@ fn power(num1: f64, thing2: f64) -> f64{
 fn evaluate(function: &str, x: f64 ) -> f64 {
     let mut substr: Vec<String> = Vec::new();
     let mut past = false;
-    let mut token_list: Vec<Chars> = Vec::new();
+    let mut token_list: Vec<Option<usize>> = Vec::new();
     //println!("{}", function);
 
-    let plus = function.find("+");
-    let minus = function.find("-");
-    let multiply = function.find("*");
-    let divide = function.find("/");
-    let exponent = function.find("^");
+    token_list.push(function.find("+"));
+    token_list.push(function.find("-"));
+    token_list.push(function.find("*"));
+    token_list.push(function.find("/"));
+    token_list.push(function.find("^"));
     let paren = function.find("(");
 
-
-    if plus != None {
-        substr.push(function.chars().skip(0).take(plus.unwrap()).collect());
-        substr.push(function.chars().skip(plus.unwrap()).take(1).collect());
-        substr.push(function.chars().skip(plus.unwrap() + 1).take(function.chars().count() - plus.unwrap()).collect());
-        past = true;
+    for token in token_list{
+        if token != None{
+            substr.push(function.chars().skip(0).take(token.unwrap()).collect());
+            substr.push(function.chars().skip(token.unwrap()).take(1).collect());
+            substr.push(function.chars().skip(token.unwrap() + 1).take(function.chars().count() - token.unwrap()).collect());
+            past = true;
+            break;
         }
-    else if minus !=None {
-        substr.push(function.chars().skip(0).take(minus.unwrap()).collect());
-        substr.push(function.chars().skip(minus.unwrap()).take(1).collect());
-        substr.push(function.chars().skip(minus.unwrap() + 1).take(function.chars().count() - minus.unwrap()).collect());
-        past = true;
-    }
-    else if multiply != None {
-        substr.push(function.chars().skip(0).take(multiply.unwrap()).collect());
-        substr.push(function.chars().skip(multiply.unwrap()).take(1).collect());
-        substr.push(function.chars().skip(multiply.unwrap() + 1).take(function.chars().count() - multiply.unwrap()).collect());
-        past = true;
-    }
-    else if divide != None {
-        substr.push(function.chars().skip(0).take(divide.unwrap()).collect());
-        substr.push(function.chars().skip(divide.unwrap()).take(1).collect());
-        substr.push(function.chars().skip(divide.unwrap() + 1).take(function.chars().count() - divide.unwrap()).collect());
-        past = true;
-    }
-    else if exponent != None{
-        substr.push(function.chars().skip(0).take(exponent.unwrap()).collect());
-        substr.push(function.chars().skip(exponent.unwrap()).take(1).collect());
-        substr.push(function.chars().skip(exponent.unwrap() + 1).take(function.chars().count() - exponent.unwrap()).collect());
-        past = true;
     }
 
     if past {
